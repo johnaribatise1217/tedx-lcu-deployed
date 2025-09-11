69e1092e4a90b5bda918c1d1645e6710ad85a3b6
Aribatise John Olugbadeleke. <johnaribatise@hsass.org>
2025-09-11 20:10:13 +0100

    Update layout.js corrected suspense

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
-import { Suspense } from "react
+import { Suspense } from "react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
